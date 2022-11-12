9d02896c681f2601ca3d7ceb9a9f425b3b2244b2
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2022-11-12 12:17:40 +0600

    add somthing

--- pages/account.tsx
+++ pages/account.tsx
@@ -3,6 +3,9 @@ import { useSession } from 'next-auth/react';
 
 const Account = () => {
 
+    const{data:session}=useSession();
+    console.log(session);
+    
     return (
         <div>
             <h1 className='text-center text-green-500 text-3xl mt-5 font-bold'>THis is account Page</h1>
